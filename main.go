967be5b152d6bcf3848371cb38bdbabcbd3f1c28
mell <chanon4817@gmail.com>
2023-01-27 15:30:32 +0700

    feat: repo - db adapter

--- main.go
+++ main.go
@@ -1,5 +1,36 @@
 package main
 
-func main() {
+import (
+	"bank/repository"
+	"fmt"
+	"os"
 
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jmoiron/sqlx"
+)
+
+func main() {
+	url := os.Getenv("DATABASE_URL")
+	db, err := sqlx.Open("mysql", url)
+	if err != nil {
+		panic(err)
+	}
+
+	customerRepository := repository.NewCustomerRepositoryDB(db)
+
+	_ = customerRepository
+
+	customer, err := customerRepository.GetById(2000)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(customer)
+
+
+	// customers, err := customerRepository.GetAll()
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// fmt.Println(customers)
 }
